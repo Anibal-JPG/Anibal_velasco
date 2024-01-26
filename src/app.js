e76117a08dcbe312d063b6b02491f15a67d8a117
Anibal-JPG <109487916+Anibal-JPG@users.noreply.github.com>
2024-01-26 03:19:56 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = ["CartMan", "kyle", "kenny", "butters"];
+  let cuando = ["ayer", "en mañana", "en la tarde", "antier"];
+  let accion = ["cayo", "volo", "mojo", "le cayo un rayo"];
+  let que = ["el perro", "el  gato", "el tiburo", "la tortuga", "mi hermana"];
+
+  let randomQuien = quien[Math.floor(Math.random() * quien.length)];
+  let randomCuando = cuando[Math.floor(Math.random() * cuando.length)];
+  let randomAccion = accion[Math.floor(Math.random() * accion.length)];
+  let randomque = que[Math.floor(Math.random() * que.length)];
+
+  console.log(
+    randomQuien,
+    " " + randomCuando,
+    " " + randomAccion,
+    " " + randomque,
+    ":D"
+  );
 };
